7dbd1aca8f2ece5e3352137aaee83ba496d92dba
Li Jun LM Peng <pengljun@cn.ibm.com>
2021-08-26 11:34:49 +0800

    add class in js 1

--- signal.js
+++ signal.js
@@ -6,3 +6,5 @@ $("#header").addClass("site-menu hover py-3 py-lg-0");
 $("#navigator").addClass("aside-menu");
 $("#navList").addClass("menu-inner");
 $("#navList li").addClass("menu-item menu-item-type-taxonomy menu-item-object-category current-post-ancestor current-menu-parent current-post-parent in menu-item-1911 nav-item");
+$("#navList li a").addClass('nav-link');
+$("#navList li a").addClass('nav-text');
